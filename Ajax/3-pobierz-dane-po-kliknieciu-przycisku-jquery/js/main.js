a54e41b05e11efad4d68c52f4b1881c472daa15c
Marcin <lisowskim@gmail.com>
2020-06-22 15:49:06 +0200

    Dodanie zadania 3-AJAX

---
+++ Ajax/3-pobierz-dane-po-kliknieciu-przycisku-jquery/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  
+    $('#btn-put').click(function (){
+
+        $.get('https://akademia108.pl/api/ajax/get-post.php')
+        .done(function(data){
+
+            let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+            let userId = $('<p></p>').text(`Post ID: ${data.userId}`);
+            let tite = $('<p></p>').text(`Post ID: ${data.tite}`);
+            let body = $('<p></p>').text(`Post ID: ${data.body}`);
+
+            $('body').append(pId);
+            $('body').append(userId);
+            $('body').append(tite);
+            $('body').append(body);
+
+            console.log(data);
+        })
+        .fail(function(error){
+            console.error(error);
+            
+        })
+
+
+        console.log(`Działam`);
+    })
+    
+});
+
